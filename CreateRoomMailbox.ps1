9c12b5eb14a96a84e154e994b2cc59485f3cfca7
Ali Asghar <aliasgherit@gmail.com>
2025-05-20 04:12:52 +0500

    Update CreateRoomMailbox.ps1

--- CreateRoomMailbox.ps1
+++ CreateRoomMailbox.ps1
@@ -1,7 +1,7 @@
-$AppId = "24560a07-db76-4d8d-ba93-21ebbefaffea"
-$Tenant = "isb0.onmicrosoft.com"
-$CertThumbprint = "B74D418442756942591C0BD2455406EA72A3F295"
-Connect-ExchangeOnline -AppId $AppId -Organization $Tenant -CertificateThumbprint $CertThumbprint
+
+
+Connect-ExchangeOnline -AppId $env:EXO_APP_ID -Organization $env:EXO_TENANT_ID -CertificateThumbprint $env:EXO_CERT_THUMBPRINT
+
 
 # Import CSV
 $mailboxes = Import-Csv -Path "./UsersToMigrate.csv"
